aac6a6fbc0ef0e68ba612c25a154ddd831124bb4
Leila DQIQI <leila.dqiqi@yahoo.fr>
2018-06-05 15:16:23 +0200

    i have center the searchbar

--- front-booker/src/components/SearchBar.js
+++ front-booker/src/components/SearchBar.js
@@ -3,43 +3,52 @@ import PropTypes from 'prop-types'
 import { withStyles } from '@material-ui/core/styles'
 import SearchBar from 'material-ui-search-bar'
 import Button from '@material-ui/core/Button'
-// import Paper from '@material-ui/core/Paper'
+import Paper from '@material-ui/core/Paper'
 import Grid from '@material-ui/core/Grid'
 
-const styles = {
+const styles = theme => ({
   root: {
+    flexGrow: 1
+  },
+  paper: {
+    padding: theme.spacing.unit * 2,
     textAlign: 'center'
   }
-}
+})
 
 function SimpleSearchBar (props) {
   const { classes } = props
   return (
     <div className={classes.root}>
-      <Grid container spacing={24}>
-        <Grid item xs={12}>
-          <Grid
-            container
-            spacing={10}
-            className={classes.demo}
-            direction={'row'}
-            justify={'center'}
-          >
-            <form>
-              <h1> RESERVEZ VOTRE SESSION SPORTIVE EN LIGNE </h1>
-              <Grid item xs={6}>
-                <SearchBar className={classes.search} position="static" color="default"
-                  onChange={() => console.log('onChange')}
-                  onRequestSearch={() => console.log('onRequestSearch')} />
+      <Grid item xs={12}>
+        <Grid
+          container
+          spacing={10}
+          className={classes.demo}
+          direction={'row'}
+          justify={'center'}
+        >
+          <form>
+            <h1> RESERVEZ VOTRE SESSION SPORTIVE EN LIGNE </h1>
+
+            <Grid container spacing={24}>
+              <Grid item xs={4}>
                 <SearchBar className={classes.search} position="static" color="default"
                   onChange={() => console.log('onChange')}
                   onRequestSearch={() => console.log('onRequestSearch')} />
               </Grid>
-              <Button className={classes.btn} variant="raised" style={{backgroundColor: '#66FF33'}}>
+              <Grid item xs={4}>
+                <SearchBar className={classes.search} position="static" color="default"
+                  onChange={() => console.log('onChange')}
+                  onRequestSearch={() => console.log('onRequestSearch')} />
+              </Grid>
+              <Grid item xs={4}>
+                <Button className={classes.btn} variant="raised" style={{backgroundColor: '#66FF33'}}>
                 Recherchez
-              </Button>
-            </form>
-          </Grid>
+                </Button>
+              </Grid>
+            </Grid>
+          </form>
         </Grid>
       </Grid>
     </div>
