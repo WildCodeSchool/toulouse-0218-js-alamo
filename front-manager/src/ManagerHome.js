15f7c41a7402b3f90ed49bd215b83d09371da83b
Romain Vaur <romainwcs@gmail.com>
2018-06-07 16:43:40 +0200

    composant login cote manager

--- front-manager/src/ManagerHome.js
+++ front-manager/src/ManagerHome.js
@@ -3,7 +3,10 @@ import List from '@material-ui/core/List';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import { withStyles } from '@material-ui/core';
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
+import TextField from '@material-ui/core/TextField';
+import FormControl from '@material-ui/core/FormControl';
+import Button from '@material-ui/core/Button';
 
 const styles = theme => ({
   paper: {
@@ -11,18 +14,58 @@ const styles = theme => ({
     margin: theme.spacing.unit * 2,
     marginTop: 100,
     color: theme.palette.text.secondary,
-    height: 550
+    height: 350,
+  },
+  textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit,
+    width: 200,
+  },
+  button: {
+    margin: theme.spacing.unit,
+    marginTop: 50
   },
 })
 
 class ManagerHome extends React.Component {
+
+  handleChange = name => event => {
+    this.setState({
+      [name]: event.target.value,
+    });
+  };
+
   render() {
     const { classes } = this.props
     return (
       <div>
-        <Grid item xs={4}>
-          <Paper className={classes.paper}>login
-          </Paper>
+        <Grid container justify={'center'}>
+          <Grid item xs={4} >
+            <Paper className={classes.paper}>
+              <FormControl className={classes.container} noValidate autoComplete="off">
+                <p>Login</p>
+                <TextField
+                  id="name"
+                  label="Nom du club"
+                  className={classes.textField}
+                  placeholder=" "
+                  onChange={this.handleChange('name')}
+                  margin="normal"
+                />
+                <TextField
+                  id="password-input"
+                  label="Password"
+                  className={classes.textField}
+                  type="password"
+                  onChange={this.handleChange('password')}
+                  margin="normal"
+                />
+                <Button color="primary" className={classes.button}>
+                  Login
+                </Button>
+              </FormControl>
+            </Paper>
+          </Grid>
         </Grid>
       </div>
     )
