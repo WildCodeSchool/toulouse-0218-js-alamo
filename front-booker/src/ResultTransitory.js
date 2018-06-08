7efe3f0d64dc5c1ed48ae14095cce1595e0abb7c
Alexandre Morazin <alexandre.morazin@live.fr>
2018-06-08 16:11:27 +0200

    add default date on textfield

--- front-booker/src/ResultTransitory.js
+++ front-booker/src/ResultTransitory.js
@@ -1,15 +1,57 @@
 import React from 'react'
+import TextField from '@material-ui/core/TextField'
+import {withStyles} from '@material-ui/core/styles'
+import Grid from '@material-ui/core/Grid'
+import PropTypes from 'prop-types'
+
+const styles = theme => ({
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap'
+  },
+  textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit,
+    width: 200
+  }
+})
 
 class ResultTransitory extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {}
+  }
+
   render () {
+    const { classes } = this.props
+    const date = new Date()
+    const defaultDate = date.toISOString().substr(0, 10)
     return (
       <div>
-        <p>Filtrer par:</p>
-        <input className="date" type="date" placeholder="Date"/>
-        <button>Appliquer</button>
+        <div>
+          <form className={classes.container} noValidate>
+            <TextField
+              id="date"
+              label="Date"
+              type="date"
+              defaultValue={defaultDate}
+              className={classes.textField}
+              InputLabelProps={{
+                shrink: true
+              }}
+            />
+          </form>
+        </div>
+        <div>
+          <Grid container>
+          </Grid>
+        </div>
       </div>
     )
   }
 }
-
-export default ResultTransitory
+ResultTransitory.propTypes = {
+  classes: PropTypes.object,
+  nextStep: PropTypes.func
+}
+export default withStyles(styles)(ResultTransitory)
