cd41ba1e82e21fbc59486e0e7f05f5c49ee5f488
Benoît Hubert <benoithubert@gmail.com>
2018-07-05 16:39:41 +0200

    Handle real dates

--- front-booker/src/components/WeekCalendar/index.js
+++ front-booker/src/components/WeekCalendar/index.js
@@ -3,7 +3,7 @@ import React from 'react'
 const CAL_HEIGHT = 300
 const days = ['Lu', 'Ma', 'Me', 'Je', 'Ve', 'Sa', 'Di']
 const dayHeadStyles = {width: '9%'}
-const dayStyles = {width: '13%', height: `${CAL_HEIGHT}px`, fontSize: '11px', position: 'relative'}
+const dayStyles = {width: '13%', height: `${CAL_HEIGHT}px`, fontSize: '11px', position: 'relative', textAlign: 'center'}
 
 const HOUR_MIN = 360
 
@@ -38,7 +38,8 @@ const getSlotStyle = ts => {
 
 const WeekDay = props => (
   <div style={dayStyles}>
-    {props.day}
+    <div>{props.day}</div>
+    <div>{props.date.getDate()}</div>
     {
       props.timeSlots.map(
         (ts, k) => <div key={k} style={getSlotStyle(ts)}>
@@ -49,20 +50,61 @@ const WeekDay = props => (
   </div>
 )
 
+const getDates = nowDate => {
+  const nowDow = nowDate.getDay()
+  const dates = []
+  for(let d = 1 ; d < 8 ; d++) {
+    const date = new Date(nowDate)
+    date.setDate(date.getDate() + d - nowDow)
+    dates.push(date)
+  }
+  return dates
+}
+
 class WeekCalendar extends React.Component {
+  constructor (props) {
+    super(props)
+    const now = new Date()
+    this.state = {
+      weekIndex: 0,
+      now,
+      dates: getDates(now)
+    }
+  }
+  weekChange = increment => {
+    this.setState((prevState, props) => {
+      const weekIndex = prevState.weekIndex + increment
+      const now = new Date()
+      let day = now.getDate()
+      day += weekIndex * 7
+      now.setDate(day)
+      const dates = getDates(now)
+      return {
+        weekIndex,
+        now,
+        dates
+      }
+    })
+  }
   render () {
+    const { weekIndex, dates } = this.state
+    const sow = dates[0]
+    const sowStr = `${sow.getDate()}/${sow.getMonth() + 1 }`
+    const eow = dates[6]
+    const eowStr = `${eow.getDate()}/${eow.getMonth() + 1 }`
     return (
       <div>
         <div style={{display: 'flex'}}>
-          <div style={{width: '50%', textAlign: 'left'}}>&laquo;</div>
-          <div style={{width: '50%', textAlign: 'right'}}>&raquo;</div>
+          <div style={{width: '20%', textAlign: 'left'}}><button onClick={() => this.weekChange(-1)} disabled={weekIndex === 0}>&laquo;</button></div>
+          <div style={{width: '60%', textAlign: 'center'}}>{sowStr} &mdash; {eowStr}</div>
+          <div style={{width: '20%', textAlign: 'right'}}><button onClick={() => this.weekChange(1)}>&raquo;</button></div>
         </div>
         <div style={{display: 'flex'}}>
           <div style={dayHeadStyles}>
           </div>
           {
             days.map((d, k) => (
-              <WeekDay key={k} day={d} timeSlots={getTimeSlots(k, this.props.timeSlots)} />
+              <WeekDay key={k} day={d} date={this.state.dates[k]} timeSlots={getTimeSlots(k, this.props.timeSlots)} />
             ))
           }
         </div>
