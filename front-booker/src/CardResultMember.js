b329852d1c2fcfc4bf7dc9f4eb499bdee2a5f4ea
Benoît Hubert <benoithubert@gmail.com>
2018-07-06 10:00:53 +0200

    Change disabled/enabled booking button state, according to selected time slot

--- front-booker/src/CardResultMember.js
+++ front-booker/src/CardResultMember.js
@@ -9,17 +9,18 @@ import PropTypes from 'prop-types'
 import { Link, withRouter } from 'react-router-dom'
 import MyCalendar from '../src/components/MyCalendar'
 import WeekCalendar from './components/WeekCalendar'
+import classNames from 'classnames'
 
 const styles = () => ({
   card: {
-    height: 300
+    // height: 300
   },
   paper: {
     height: 500,
     backgroundColor: '#E6EAF0'
   },
   paperCalendar: {
-    height: 270
+    // height: 270
   },
   verticalItems: {
     display: 'flex',
@@ -29,9 +30,15 @@ const styles = () => ({
     flexGrow: 1
   },
   button: {
-    backgroundColor: '#66FF33',
     marginRight: 20,
     marginBottom: 20,
+  },
+  disabledButton: {
+    backgroundColor: '#AAA',
+    color: '#333'
+  },
+  enabledButton: {
+    backgroundColor: '#66FF33',
     color: 'white'
   },
   link: {
@@ -43,10 +50,21 @@ const styles = () => ({
 class CardResultMember extends React.Component {
   constructor (props) {
     super(props)
-    this.state = {}
+    this.state = {
+      selectedTimeSlot: null,
+      canBook: false
+    }
+  }
+  onClickSlot = (timeSlot, isBookable, isBooked) => {
+    this.setState({
+      selectedTimeSlot: timeSlot,
+      canBook: isBookable && ! isBooked
+    })
   }
   render () {
     const { classes, timeSlots, reservations } = this.props
+    const { selectedTimeSlot, canBook } = this.state
+    const buttonClass = canBook ? classes.enabledButton : classes.disabledButton
     return (
       <div>
         <Card className={classes.card}>
@@ -61,12 +79,15 @@ class CardResultMember extends React.Component {
                   {this.props.club.email} <br />
                   {this.props.club.phone} <br />
                 </Typography>
-                <Button className={classes.button}>
-                  <Link to = {'/reservation/' + this.props.club.id} className={classes.link}>Réserver</Link>
+                <Button className={classNames(classes.button, buttonClass)} disabled={! canBook}>
+                  <Link to={'/reservation/' + this.props.club.id} className={classes.link}>Réserver</Link>
                 </Button>
               </Grid>
               <Grid item xs={6}>
-                <WeekCalendar reservations={reservations} timeSlots={timeSlots.filter(ts => ts.managerId === this.props.club.managerId)} />
+                <WeekCalendar
+                  onClickSlot={this.onClickSlot}
+                  reservations={reservations}
+                  timeSlots={timeSlots.filter(ts => ts.managerId === this.props.club.managerId)} />
               </Grid>
             </Grid>
           </CardContent>
