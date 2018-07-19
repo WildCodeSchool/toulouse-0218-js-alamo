2cca96cf352f255a0223fa8700081927c5e8cdaf
Benoît Hubert <benoithubert@gmail.com>
2018-07-19 14:57:10 +0200

    fix ESLint warnings

--- front-booker/src/CardResultEmpty.js
+++ front-booker/src/CardResultEmpty.js
@@ -1,10 +1,8 @@
 import React from 'react'
 import { withStyles } from '@material-ui/core/styles'
-import Grid from '@material-ui/core/Grid'
 import Card from '@material-ui/core/Card'
 import CardContent from '@material-ui/core/CardContent'
 import Typography from '@material-ui/core/Typography'
-import Paper from '@material-ui/core/Paper'
 import PropTypes from 'prop-types'
 
 const styles = theme => ({
